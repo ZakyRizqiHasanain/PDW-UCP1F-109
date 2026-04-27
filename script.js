bc6660c22e86fb1416aac0d4da07894280ad2af1
ZakyRizqiHasanain <zaky.rizqi.ft24@mail.umy.ac.id>
2026-04-27 08:10:29 +0700

    penambahan file js untuk interaksi website

---
+++ script.js
@@ -0,0 +1,67 @@
+// ARRAY DATA
+let members = [];
+
+// LOAD TABLE
+function renderTable() {
+    let table = document.getElementById("memberTable");
+    if (!table) return;
+
+    table.innerHTML = "";
+
+    members.forEach(m => {
+        table.innerHTML += `
+            <tr>
+                <td>${m.name}</td>
+                <td>${m.email}</td>
+                <td>${m.interest}</td>
+            </tr>
+        `;
+    });
+}
+
+// FORM
+document.addEventListener("DOMContentLoaded", function () {
+    let form = document.getElementById("registrationForm");
+
+    if (form) {
+        form.addEventListener("submit", function (e) {
+            e.preventDefault();
+
+            let name = document.getElementById("name").value;
+            let email = document.getElementById("email").value;
+            let interest = document.getElementById("interest").value;
+
+            let data = { name, email, interest };
+
+            members.push(data);
+
+            // tampilkan hasil
+            document.getElementById("displayText").innerHTML =
+                `Nama: ${name} <br>Email: ${email} <br>Minat: ${interest}`;
+
+            document.getElementById("resultArea").style.display = "block";
+
+            alert("Data berhasil disimpan!");
+
+            renderTable();
+
+            form.reset();
+        });
+    }
+
+    renderTable();
+});
+
+// MULTIMEDIA
+function changeImage() {
+    document.getElementById("techImage").src =
+        "https://picsum.photos/400/250?random=" + Math.random();
+}
+
+function playAudio() {
+    document.getElementById("myAudio").play();
+}
+
+function pauseAudio() {
+    document.getElementById("myAudio").pause();
+}
